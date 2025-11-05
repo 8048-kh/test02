3cdd274619ae36aca1e16a6be117a3fbc5cbc6bd
8048-kh <8046khtotn@gmail.com>
2025-11-05 21:01:19 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,7 +48,7 @@ tribe_names.sort()
 
 # Streamlit interface
 selected_tribe = st.selectbox(
-    "選擇主要部落 (N_Tribe)", tribe_names, key="selectbox_tribe"
+    "選擇部落 (N_Tribe)", tribe_names, key="selectbox_tribe"
 )
 
 # Filter data
