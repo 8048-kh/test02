96fb5efa7efa050e533d62017838c415ff304154
8048-kh <8046khtotn@gmail.com>
2025-11-05 20:12:45 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,9 +23,7 @@ m = leafmap.Map(center=[23.97565, 120.9738819], zoom=4)
 
 # Load the tribes data
 tribes = "https://github.com/8048-kh/test02/raw/refs/heads/main/tribe.csv"
-#tribes_df = pd.read_csv(tribes, encoding='latin-1')
 tribes_df = pd.read_csv(tribes)
-#tribes_df = pd.read_csv(tribes, encoding='latin-1')
 tribe_names = tribes_df['N_Tribe'].tolist()
 
 # Add points to the map
@@ -35,7 +33,7 @@ selected_tribe = st.selectbox(
 )
 
 # Get the data of the selected tribe
-selected_tribe_data = tribes_df[tribes_df['tribe name'] == selected_tribe].iloc[0]
+selected_tribe_data = tribes_df[tribes_df['N_Tribe'] == selected_tribe].iloc[0]
 
 # Get coordinates from 'latitude' and 'longitude'
 latitude = selected_tribe_data['NT_lat']
